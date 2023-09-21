fbd058282dd52f58ca4715cf3a2fa4bda819ca3d
dani-jimlar <143829673+dani-jimlar@users.noreply.github.com>
2023-09-21 21:33:00 +0000

    change in import

--- source/script.py
+++ source/script.py
@@ -1,14 +1,14 @@
 """
 This script uses functions from lib.py to generate a brief analysis of a dataset
 """
-import sys
-sys.path.append('source')
-from lib import print_ds_info 
-from lib import view_data_vis 
-from lib import save_data_vis 
-
 
 import pandas as pd 
+from source.lib import print_ds_info 
+from source.lib import view_data_vis 
+
+from source.lib import save_data_vis 
+#import sys
+#sys.path.append('source')
 
 
 dt= pd.read_csv("source/imp_edos_2.csv", encoding="ISO-8859-1")
