4210aecd0cb5a9f414d60476ae636b99438053e6
dani-jimlar <143829673+dani-jimlar@users.noreply.github.com>
2023-09-23 22:24:03 +0000

    update scrpt

--- source/script.py
+++ source/script.py
@@ -1,17 +1,17 @@
 """
-This script uses functions from lib.py to generate a brief analysis of a dataset
+This script uses functions from lib.py to generate a brief analysis of a dataset and save a data viszualization of the data
 """
 import pandas as pd 
-import source.lib 
+from source import lib 
 dt= pd.read_csv("source/imp_edos_2.csv", encoding="ISO-8859-1")
 # Print dataset info
-print_ds_info(dt)
+lib.print_ds_info(dt)
 # view and save graph
-view_data_vis(dt)
-save_data_vis(dt)
+lib.view_data_vis(dt)
+lib.save_data_vis(dt)
 
 #Print descriptive statistics of just one variable.
 selected=dt.iloc[:,3]
 
-print_min(selected)
+lib.print_min(selected)
 
