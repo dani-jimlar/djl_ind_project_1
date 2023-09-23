58a78b5171db1754f7cef4d9d90f3bb39747bce4
dani-jimlar <143829673+dani-jimlar@users.noreply.github.com>
2023-09-23 04:52:16 +0000

    update

--- source/test_lib.py
+++ source/test_lib.py
@@ -2,17 +2,6 @@
 from source import lib
 import pandas as pd
 
-def test_print_mean(selected):
-    ''' doc string'''
-    assert lib.print_mean(selected) == "The mean is 740.56"
-
-def test_print_median(selected):
-    '''doc string'''
-    assert lib.print_median(selected) == "The median is 185.5"
-
-def test_print_max(selected):
-    '''doc string'''
-    assert lib.print_max(selected) == "The max is 22782"
 
 def test_print_min(selected):
     '''doc string'''
@@ -24,7 +13,7 @@ if __name__ == "__main__":
     #sel = pd.read_csv("source/imp_edos_2.csv", encoding="ISO-8859-1").iloc[:,3]
     sel = pd.read_csv("source/imp_edos_2.csv", encoding="ISO-8859-1").iloc[:,3]
     #print(sel)
-    test_print_mean(sel)
+    #test_print_max(sel)
     #test_print_median(sel)
     #test_print_max(sel)
     #assert lib.print_min(sel) == "The min is 0"
