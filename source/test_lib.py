b5e549f2b30947870f0b3dce4842463a82cb6c32
dani-jimlar <143829673+dani-jimlar@users.noreply.github.com>
2023-09-23 05:29:02 +0000

    updates

--- source/test_lib.py
+++ source/test_lib.py
@@ -9,15 +9,5 @@ def test_print_min(selected):
     assert lib.print_min(selected) == "The min is 0"
 
 if __name__ == "__main__":
-    # Print dataset infoß
-    #sel = pd.read_csv("source/imp_edos_2.csv", encoding="ISO-8859-1").iloc[:,3]
     sel = pd.read_csv("source/imp_edos_2.csv", encoding="ISO-8859-1").iloc[:,3]
-    #print(sel)
-    #test_print_max(sel)
-    #test_print_median(sel)
-    #test_print_max(sel)
-    #assert lib.print_min(sel) == "The min is 0"
-
     test_print_min(sel)
-    #min_val = lib.print_min(sel)
-    #assert min_val == 0
--- source/test_script.py
+++ source/test_script.py
@@ -1,14 +1,24 @@
-
+from source import lib
+import pandas as pd
 import requests
-from script import aggregate_statistics
+
+
+
+def test_print_min(selected):
+    '''doc string'''
+    #print(lib.print_min(selected))
+    assert lib.print_min(selected) == "The min is 0"
 
 
 def test_github_file_existence():
-    name = "nogibjj" 
-    repo = "IDS706_DataEngineering_BarbaraFlores_Project1"  
-    path_to_file = "output/Total_applicants.png"  
+    name = "dani-jimlar" 
+    repo = "djl_ind_project_1"  
+    path_to_file = "source/bar_plot2.png"  
     url = f"https://api.github.com/repos/{name}/{repo}/contents/{path_to_file}"
-    
     response1 = requests.get(url)
-    
     assert response1.status_code == 200
+
+if __name__ == "__main__":
+    sel = pd.read_csv("source/imp_edos_2.csv", encoding="ISO-8859-1").iloc[:,3]
+    test_github_file_existence()
+    test_print_min(sel)    
